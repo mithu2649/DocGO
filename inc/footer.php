7ad0f07f60bf30e7c5cc1ad96273c912841e0524
mithu2649 <mithu2649@gmail.com>
2020-04-08 14:11:17 +0530

    Added footer information

--- inc/footer.php
+++ inc/footer.php
@@ -4,5 +4,26 @@
                 echo '<script src="resources/js/upload.js"></script>';
             } ?>
             <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+            <div id="footer">
+                <div id="c-notice"><p>&copy; Copyright <?php echo date("Y"); ?> by Shree Ramkrishna Institute of Science and Technology.</br>All Rights Reserved.</p></div>
+                <div id="social">
+                    <span class="social-icon icon-facebook">
+                        <a href="#">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </span>
+                    <span class="social-icon icon-google">
+                        <a href="#">
+                            <i class="fa fa-google" aria-hidden="true"></i>
+                        </a>
+                    </span>
+                    <span class="social-icon icon-twitter">
+                        <a href="#">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                    </span>
+                </div>
+            </div>
         </body>
     </html>
--- inc/header.php
+++ inc/header.php
@@ -14,6 +14,7 @@ if ($currentPage  != 'login' && $currentPage != 'register') {
     <title>DocGo - Document Sharing Platform</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="resources/css/header.css">
+    <link rel="stylesheet" href="resources/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet">
     <link href="resources/icons/document.png" rel="icon" type="image/x-icon" />
     <?php
